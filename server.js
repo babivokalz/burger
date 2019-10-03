b6654fb4582dfd5afbbe61c4c1c022bb9a8adeda
AlaCrity <wlchen1105@gmail.com>
2019-10-02 22:26:54 -0400

    mostly finished the app, still working on styling and getting the buttons to work

--- server.js
+++ server.js
@@ -6,13 +6,17 @@ var exphbs = require("express-handlebars");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-  res.send("route hit");
-});
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
